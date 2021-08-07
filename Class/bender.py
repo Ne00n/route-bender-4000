3b85881b2b38ef23c182e133db4613c1a7c23ea8
Neoon <neo@tiefkuehler-madness.me>
2021-08-07 02:40:11 +0200

    try last 2 mtr hops

--- Class/bender.py
+++ Class/bender.py
@@ -114,19 +114,22 @@ class Bender:
             print(line['ip_dst'],"not reachable, trying to MTR")
             result = self.cmd('mtr '+line['ip_dst']+' --report --report-cycles 4 --no-dns')
             parsed = re.findall("-- ([0-9.]+)",result[0], re.MULTILINE)
-            lastIP = parsed[len(parsed) -1]
-            if self.isPrivate(lastIP):
-                print(lastIP+" is private, skipping")
-                exit()
-            if lastIP != "???":
-                direct = self.cmd("fping -c6 "+lastIP)
-            if '100%' in direct[1]:
-                print(line['ip_dst'],"("+lastIP+") not reachable, skipping")
-                exit()
-            else:
-                origin = line['ip_dst']
-                line['ip_dst'] = lastIP
-
+            for run in range(1,3):
+                lastIP = parsed[len(parsed) - run]
+                if self.isPrivate(lastIP):
+                    print(lastIP+" is private, skipping")
+                    exit()
+                if lastIP != "???":
+                    direct = self.cmd("fping -c6 "+lastIP)
+                if '100%' in direct[1]:
+                    print(line['ip_dst'],"("+lastIP+") not reachable.")
+                else:
+                    origin = line['ip_dst']
+                    line['ip_dst'] = lastIP
+                    break
+                if run == 2:
+                    print("Could not find pingable IP for",line['ip_dst'])
+                    exit()
         latency,queue,outQueue,count = [],Queue(),Queue(),0
         for server in self.nodes:
             queue.put({"server":server,"ip":line['ip_dst']})
