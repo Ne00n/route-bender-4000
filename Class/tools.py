44ca62a508dda6f7f2eb0069b446e574890b7742
Neoon <neo@tiefkuehler-madness.me>
2023-08-12 17:22:16 +0200

    getAvrg return 64k on packet loss

--- Class/tools.py
+++ Class/tools.py
@@ -107,6 +107,8 @@ class Tools:
     def getAvrg(row):
         result = 0
         if not row: return 65000
+        #if we found packet loss abort
+        if float(row[len(row) -1][1]) > 0: return 64000
         for entry in row:
             #ignore timed out
             if entry[0] == "timed out": continue
