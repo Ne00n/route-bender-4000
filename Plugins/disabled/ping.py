a5c676cc051934829bac5392b19a42760c2687c3
Neoon <neo@tiefkuehler-madness.me>
2024-06-11 18:47:38 +0200

    not longer needed

--- Plugins/disabled/ping.py
+++ Plugins/disabled/ping.py
@@ -13,7 +13,7 @@ class ping():
             req = requests.get(f"https://ping.serv.app/{ip}", timeout=(3, 3))
             if req.status_code == 200:
                 data = req.json()
-                if data['next'] == False: return data['ips']
+                return data['ips']
             return False
         except Exception as err:
             return False
